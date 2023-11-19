eca0c352e2030207e8ab838dee4ccd02a4ee8546
lrnvkrt <krist1na.larion0va@yandex.ru>
2023-11-19 16:09:58 +0300

    Update README.md and Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,22 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a+b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if(b == 0) {
+            throw new IllegalArgumentException("You can't divide be zero!");
+        } else {
+            return a/b;
+        }
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a*b;
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        System.out.println("Решим уравнение 7x-1=13");
+        return div(add(13, 1), 7);
     }
 }
