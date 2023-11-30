03228a6c9596fca349ccff963eefb8077985263f
sorivma <96016733+sorivma@users.noreply.github.com>
2023-11-30 19:09:04 +0300

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -20,6 +20,6 @@ public class Calculator {
         System.out.println("Решим уравнение 5x+5 = 0");
         System.out.println("Решим уравнение 7x-1=13");
         System.out.println("Вернем среднее арифметическое");
-        return div(div(5, dif(0, 5)) + div(add(13, 1), 7), 2)
+        return div(div(5, dif(0, 5)) + div(add(13, 1), 7), 2);
     }
 }
